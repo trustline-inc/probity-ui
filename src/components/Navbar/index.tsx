ba0cce53de26e5c9bbf7c44414d555e4d7db4fb2
Tim Selaty <timselaty@gmail.com>
2021-04-13 15:16:02 -0400

    Move nav to the left

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,88 +1,113 @@
-import React from 'react';
-import useSWR from 'swr';
+import React from "react";
+import useSWR from "swr";
 import { NavLink } from "react-router-dom";
-import { useWeb3React } from '@web3-react/core'
-import { Web3Provider } from '@ethersproject/providers';
-import { formatEther } from '@ethersproject/units';
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import { formatEther } from "@ethersproject/units";
 import { injected } from "../../connectors";
 import fetcher from "../../fetcher";
 import logo from "../../assets/logo.png";
+import "./index.css";
 
 function Balance() {
-  const { account, library } = useWeb3React<Web3Provider>()
-  const { data: balance, mutate } = useSWR(['getBalance', account, 'latest'], {
+  const { account, library } = useWeb3React<Web3Provider>();
+  const { data: balance, mutate } = useSWR(["getBalance", account, "latest"], {
     fetcher: fetcher(library),
-  })
+  });
 
   React.useEffect(() => {
     if (library !== undefined) {
-      library.on('block', () => {
-        mutate(undefined, true)  // Update balance
-      })
+      library.on("block", () => {
+        mutate(undefined, true); // Update balance
+      });
 
       return () => {
-        library.removeAllListeners('block')
-      }
+        library.removeAllListeners("block");
+      };
     }
-  })
+  });
 
-  if (!balance) return null
-  return <div className="border rounded py-1 px-2">{parseFloat(formatEther(balance)).toFixed(4)} CFLR</div>
+  if (!balance) return null;
+  return (
+    <div className="border rounded py-1 px-2">
+      {parseFloat(formatEther(balance)).toFixed(4)} CFLR
+    </div>
+  );
 }
 
 function Navbar() {
-  const { chainId, activate, active } = useWeb3React<Web3Provider>()
+  const { chainId, activate, active } = useWeb3React<Web3Provider>();
 
   const onClick = () => {
-    activate(injected)
-  }
+    activate(injected);
+  };
 
   return (
-    <div className="Navbar">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#/">
-            <img src={logo} alt="Probity" height="30" className="logo" />
-            &nbsp;Probity
-          </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse mx-4" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link" activeClassName="active" to="/vault">Vault</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" activeClassName="active" to="/capital">Capital</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" activeClassName="active" to="/loans">Loans</NavLink>
-              </li>
-            </ul>
-            <form className="row gx-3 gy-2 align-items-center">
-              {
-                chainId ? (
-                  <div className="col-auto">
-                    <div className="w-100">Chain ID: {chainId}</div>
-                  </div>
-                ) : (null)
-              }
-              <div className="col-auto">
-                {
-                  active ? (
-                    <div className="w-100"><i className="inline-block far fa-dot-circle text-success" />&nbsp;Connected</div>
-                  ) : (
-                    <button className="btn btn-outline-success" type="button" onClick={onClick}>Connect</button>
-                  )
-                }
+    <nav>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#/">
+          <img src={logo} alt="Probity" height="30" className="logo" />
+          &nbsp;Probity
+        </a>
+        {/* <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button> */}
+
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/vault">
+              <i className="fas fa-vault" /> Vault
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              to="/capital"
+            >
+              <i className="fas fa-coins" /> Capital
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/loans">
+            <i className="fas fa-money-bill-wave-alt" /> Loans
+            </NavLink>
+          </li>
+        </ul>
+        <form className="row gx-3 gy-2 align-items-center">
+          {chainId ? (
+            <div className="col-auto">
+              <div className="w-100">Chain ID: {chainId}</div>
+            </div>
+          ) : null}
+          <div className="col-auto">
+            {active ? (
+              <div className="w-100">
+                <i className="inline-block far fa-dot-circle text-success" />
+                &nbsp;Connected
               </div>
-            </form>
+            ) : (
+              <button
+                className="btn btn-outline-success"
+                type="button"
+                onClick={onClick}
+              >
+                Connect
+              </button>
+            )}
           </div>
-          <Balance />
-        </div>
-      </nav>
-    </div>
+        </form>
+        <Balance />
+      </div>
+    </nav>
   );
 }
 
--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,92 +1,123 @@
-import React, { useEffect, useState } from 'react';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import { useWeb3React } from '@web3-react/core'
-import { Web3Provider } from '@ethersproject/providers';
+import React, { useEffect, useState } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 import useLocalStorageState from "use-local-storage-state";
 import { injected } from "../../connectors";
-import Navbar from '../../components/Navbar';
-import Balances from '../../components/Balances';
-import Info from '../../components/Info';
-import Capital from '../../pages/Capital';
-import Loans from '../../pages/Loans';
-import Vault from '../../pages/Vault';
-import './index.css';
+import Navbar from "../../components/Navbar";
+import Balances from "../../components/Balances";
+import Info from "../../components/Info";
+import Capital from "../../pages/Capital";
+import Loans from "../../pages/Loans";
+import Vault from "../../pages/Vault";
+import "./index.css";
 
 function App() {
   const [mobileDevice, setMobileDevice] = useState(false);
-  const { active, activate } = useWeb3React<Web3Provider>()
-  const [displayInfoAlert, setDisplayInfoAlert] = useLocalStorageState("displayInfoAlert", true);
+  const { active, activate } = useWeb3React<Web3Provider>();
+  const [displayInfoAlert, setDisplayInfoAlert] = useLocalStorageState(
+    "displayInfoAlert",
+    true
+  );
 
   const onClick = () => {
-    activate(injected)
-  }
+    activate(injected);
+  };
 
   useEffect(() => {
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
-    if(/Mobi|Android/i.test(navigator.userAgent)) {
-      setMobileDevice(true)
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+      setMobileDevice(true);
     }
   }, []);
 
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <div className="container-fluid">
-          <div className="row mt-3">
-            <div className="col-12">
-              {
-                displayInfoAlert ? (
-                  <div className="alert alert-info alert-dismissible fade show" role="alert">
-                    <strong><i className="fas fa-exclamation-circle"></i> Notice</strong> Only testnet Spark (CFLR) is currently supported as collateral. This is a pre-release alpha version and is only compatible with the Coston testnet. Do <strong>NOT</strong> try using real money. This app is currently for testing purposes only.
-                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => {
-                      setDisplayInfoAlert(false);
-                    }}></button>
-                  </div>
-                ) : (null)
-              }
-            </div>
+        <div className="d-flex ">
+          <div className="vh-100 left-nav">
+            <Navbar />
           </div>
-          <div className="row">
-            <div className="offset-md-4 col-md-4 col-sm-12">
-              {
-                !active && (
-                  <div className="alert alert-primary alert-dismissible fade show" role="alert">
-                    {
-                      mobileDevice ? (
-                        <><strong><i className="fas fa-mobile"></i></strong>&nbsp;Mobile device detected. Please use the Metamask app to connect your wallet.</>
+          <div className="flex-grow-1  vh-100 page-container">
+            <div className="container-fluid">
+              <div className="row mt-3">
+                <div className="col-12">
+                  {displayInfoAlert ? (
+                    <div
+                      className="alert alert-info alert-dismissible fade show"
+                      role="alert"
+                    >
+                      <strong>
+                        <i className="fas fa-exclamation-circle"></i> Notice
+                      </strong>{" "}
+                      Only testnet Spark (CFLR) is currently supported as
+                      collateral. This is a pre-release alpha version and is
+                      only compatible with the Coston testnet. Do{" "}
+                      <strong>NOT</strong> try using real money. This app is
+                      currently for testing purposes only.
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="alert"
+                        aria-label="Close"
+                        onClick={() => {
+                          setDisplayInfoAlert(false);
+                        }}
+                      ></button>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+              <div className="row">
+                <div className="offset-md-4 col-md-4 col-sm-12">
+                  {!active && (
+                    <div
+                      className="alert alert-primary alert-dismissible fade show"
+                      role="alert"
+                    >
+                      {mobileDevice ? (
+                        <>
+                          <strong>
+                            <i className="fas fa-mobile"></i>
+                          </strong>
+                          &nbsp;Mobile device detected. Please use the Metamask
+                          app to connect your wallet.
+                        </>
                       ) : (
-                        <><strong><i className="fas fa-plug"></i></strong> You must <a href="#!" className="alert-link" onClick={onClick}>connect your wallet</a> before using this app.</>
-                      )
-                    }
-                  </div>
-                )
-              }
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-4 col-sm-12">
-              {active && <Info />}
-            </div>
-            <div className="col-md-4 col-sm-12">
-              <Switch>
-                <Route path="/vault">
-                  <Vault />
-                </Route>
-                <Route path="/capital">
-                  <Capital />
-                </Route>
-                <Route path="/loans">
-                  <Loans />
-                </Route>
-              </Switch>
-            </div>
-            <div className="col-md-4 col-sm-12">
-              {active && <Balances />}
+                        <>
+                          <strong>
+                            <i className="fas fa-plug"></i>
+                          </strong>{" "}
+                          You must{" "}
+                          <a href="#!" className="alert-link" onClick={onClick}>
+                            connect your wallet
+                          </a>{" "}
+                          before using this app.
+                        </>
+                      )}
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-4 col-sm-12">{active && <Info />}</div>
+                <div className="col-md-4 col-sm-12">
+                  <Switch>
+                    <Route path="/vault">
+                      <Vault />
+                    </Route>
+                    <Route path="/capital">
+                      <Capital />
+                    </Route>
+                    <Route path="/loans">
+                      <Loans />
+                    </Route>
+                  </Switch>
+                </div>
+                <div className="col-md-4 col-sm-12">
+                  {active && <Balances />}
+                </div>
+              </div>
             </div>
           </div>
         </div>
