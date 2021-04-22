a0462ffce53f5b2dba0f9ccd271b880128a92786
mattrosendin <matthew.rosendin@gmail.com>
2021-04-22 14:16:09 -0700

    display heading on full row

--- src/pages/Capital/IssuanceActivity.tsx
+++ src/pages/Capital/IssuanceActivity.tsx
@@ -50,7 +50,7 @@ function IssueActivity({
         </div>
       </div>
       <div className="row">
-        <div className="col-md-6 offset-md-3">
+        <div className="col-12">
           <div className="h-100 d-flex flex-column align-items-center justify-content-center p-4 text-center">
             <div className="m-2"><span className="text-muted h6">Coll. Ratio</span><br />{collateralRatio === 0 ? null : `${collateralRatio * 100}%`}</div>
           </div>
--- src/pages/Loans/RepayActivity.tsx
+++ src/pages/Loans/RepayActivity.tsx
@@ -16,7 +16,7 @@ function RepayActivity({
   return (
     <>
       <div className="row">
-        <div className="col-6 offset-3">
+        <div className="col-12">
           <div className="h-100 d-flex flex-column align-items-center justify-content-center p-4 text-center">
             <div className="m-2"><span className="text-muted h6">New Coll. Ratio</span><br />{collateralRatio ? `${(collateralRatio * 100).toFixed(2)}%` : null}</div>
           </div>
