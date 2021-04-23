4f97c26c85134232a35e38bc27d768c48a01d640
mattrosendin <matthew.rosendin@gmail.com>
2021-04-23 11:45:15 -0700

    open links in new tab

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -4,22 +4,22 @@ import "./index.css";
 function SocialLinks() {
   return (
     <div className="social-links mt-auto container-fluid text-center">
-      <a href="https://twitter.com/TrustlineInc" className="mx-2">
+      <a target="blank" href="https://twitter.com/TrustlineInc" className="mx-2">
         <i className="fab fa-twitter"></i>
       </a>
-      <a href="https://discord.gg/pqjAASCja8" className="mx-2">
+      <a target="blank" href="https://discord.gg/pqjAASCja8" className="mx-2">
         <i className="fab fa-discord"></i>
       </a>
-      <a href="https://t.me/TrustlineInc" className="mx-2">
+      <a target="blank" href="https://t.me/TrustlineInc" className="mx-2">
         <i className="fab fa-telegram"></i>
       </a>
-      <a href="https://medium.com/@trustlinefinance" className="mx-2">
+      <a target="blank" href="https://medium.com/@trustlinefinance" className="mx-2">
         <i className="fab fa-medium"></i>
       </a>
-      <a href="https://github.com/trustline-inc" className="mx-2">
+      <a target="blank" href="https://github.com/trustline-inc" className="mx-2">
         <i className="fab fa-github"></i>
       </a>
-      <a href="https://docs.trustline.co/trustline/" className="mx-2">
+      <a target="blank" href="https://docs.trustline.co/trustline/" className="mx-2">
         <i className="fa fa-book"></i>
       </a>
     </div>
