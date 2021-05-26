3ba0856e828869264a07d43bf97dc456761c2c63
mattrosendin <matthew.rosendin@gmail.com>
2021-05-26 15:29:01 -0700

    update bridge contract address

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
  * Coston 2 Testnet Aurei addresses
  */
 export const AUREI_ADDRESS    = process.env && process.env.REACT_APP_AUREI_ADDRESS    ? process.env.REACT_APP_AUREI_ADDRESS    : "0xf756fA532845c4Ac84609F31f7341A63Ee5B72A0";
-export const BRIDGE_ADDRESS   = process.env && process.env.REACT_APP_BRIDGE_ADDRESS   ? process.env.REACT_APP_BRIDGE_ADDRESS   : "0x5cbDbC5Cc725103D890B7cC1a22eB6DCe3D9a57C";
+export const BRIDGE_ADDRESS   = process.env && process.env.REACT_APP_BRIDGE_ADDRESS   ? process.env.REACT_APP_BRIDGE_ADDRESS   : "0x9C3055Cf42D640bBda4BbFA5AA238D16827601Ca";
 export const FTSO_ADDRESS     = process.env && process.env.REACT_APP_FTSO_ADDRESS     ? process.env.REACT_APP_FTSO_ADDRESS     : "0x32dfE359aa1E100Ba8CfBF5f38BcBdb3f55e8a06";
 export const REGISTRY_ADDRESS = process.env && process.env.REACT_APP_REGISTRY_ADDRESS ? process.env.REACT_APP_REGISTRY_ADDRESS : "0x0b4A04860d90f192eF0f399867991f35B957ed2b";
 export const TCNTOKEN_ADDRESS = process.env && process.env.REACT_APP_TCNTOKEN_ADDRESS ? process.env.REACT_APP_TCNTOKEN_ADDRESS : "0x36f86C3046503d8d246FAAf3E986705AaF3c22eD"
