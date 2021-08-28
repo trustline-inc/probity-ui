3ddcd9d44cab973baf6ca7732f2c69f986f538c5
Matthew Rosendin <matthew.rosendin@gmail.com>
2021-08-28 16:01:36 -0700

    update social links

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -4,16 +4,16 @@ import "./index.css";
 function SocialLinks() {
   return (
     <div className="social-links mt-auto container-fluid text-center">
-      <a target="blank" href="https://twitter.com/TrustlineInc" className="mx-2">
+      <a target="blank" href="https://twitter.com/ProbityDAO" className="mx-2">
         <i className="fab fa-twitter"></i>
       </a>
-      <a target="blank" href="https://discord.gg/pqjAASCja8" className="mx-2">
+      <a target="blank" href="https://discord.gg/pfz6h2TwMk" className="mx-2">
         <i className="fab fa-discord"></i>
       </a>
-      <a target="blank" href="https://t.me/TrustlineInc" className="mx-2">
+      <a target="blank" href="https://t.me/joinchat/ldcoqlTUyLIyNDIx" className="mx-2">
         <i className="fab fa-telegram"></i>
       </a>
-      <a target="blank" href="https://medium.com/@trustlinefinance" className="mx-2">
+      <a target="blank" href="https://trustline-inc.medium.com/probity-is-available-on-the-testnet-bf87d287173d" className="mx-2">
         <i className="fab fa-medium"></i>
       </a>
       <a target="blank" href="https://github.com/trustline-inc" className="mx-2">
