d22320b41763be2f4e2e1124ad68cce061adbe24
Matthew Rosendin <matthew.rosendin@gmail.com>
2021-09-17 12:46:33 -0700

    fix method name

--- src/pages/Transfers/index.tsx
+++ src/pages/Transfers/index.tsx
@@ -117,7 +117,7 @@ export default function Transfers() {
         {active && <Info />}
       </header>
       <p className="lead">Send Aurei to the Trustline App</p>
-      <p className="text-secondary"><span className="fa fa-info-circle"></span> This action will ask you to sign an <code>Approve</code> and a <code>transferAureiToXRP</code> transaction.</p>
+      <p className="text-secondary"><span className="fa fa-info-circle"></span> This action will ask you to sign an <code>Approve</code> and a <code>transferAureiToXRPL</code> transaction.</p>
       <section className="border rounded p-5 mb-5 shadow-sm bg-white">
         <Activity active={active} activity={ActivityType.Transfer} error={error}>
           <div className="row">
