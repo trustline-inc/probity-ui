9c6187e4f15c29435eee7294c9811ba739c63f34
Matthew Rosendin <matt@trustline.co>
2021-10-19 15:18:44 -0700

    parse collateral, treasury, and teller txs

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,262 +1,79 @@
-import web3 from "web3"
-import { BigNumber, Contract, utils } from "ethers"
+import { Contract, utils } from "ethers"
 import React, { useContext } from "react"
+import { Accordion, useAccordionButton } from "react-bootstrap";
 import { useWeb3React } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 import Info from '../../components/Info';
 import EventContext from "../../contexts/TransactionContext"
-import numeral from "numeral";
 import { INTERFACES } from "../../constants";
+
+function RowToggle({ eventKey, name, tx }: any) {
+  const decoratedOnClick = useAccordionButton(eventKey);
+
+  return (
+    <tr
+      className="table-primary d-flex text-center"
+      role="button"
+      onClick={decoratedOnClick}
+    >
+      <th className="col-1" scope="row">{Number(eventKey) + 1}</th>
+      <td className="col-3"><span className="badge rounded-pill bg-primary">{name}</span></td>
+      <td className="col-1">{tx.blockNumber}</td>
+      <td className="col-7 text-truncate"><code>{tx.transactionHash}</code></td>
+    </tr>
+  );
+}
 
 export default function Transactions() {
   const { active, library } = useWeb3React<Web3Provider>();
   const ctx = useContext(EventContext)
   const rows = ctx.transactions.map((tx: any, index) => {
-    // const contract = new Contract(tx.to, INTERFACES[tx.to].abi, library?.getSigner())
-    const event = tx.events.find((event: any) => event.event)
-    console.log("event:", event)
-    if (event) {
-      // const parsedEvent = contract.interface.parseLog(event);
-      // console.log("parsed:", parsedEvent)
-      // if (parsedEvent) {
-        // console.log(web3.utils.toAscii(parsedEvent.args[0]))
-      // }
-    }
-    if (event === undefined) return null;
+    const contract = new Contract(tx.to, INTERFACES[tx.to].abi, library?.getSigner())
+    const INDEX = tx.logs.length > 1 ? 1: 0 // Change this later
+    let log = contract.interface.parseLog(tx.logs[INDEX]);
+    const name = log.name
+    // console.log(name)
+    // console.log(log.args)
+
     return (
       <React.Fragment key={index}>
-        <tr className="table-primary d-flex text-center" role="button" data-bs-toggle="collapse" data-bs-target={`#collapse-${index}`}>
-          <th className="col-1" scope="row">{index + 1}</th>
-          <td className="col-3"><span className="badge rounded-pill bg-primary">{event.event}</span></td>
-          <td className="col-1">{tx.blockNumber}</td>
-          <td className="col-7 text-truncate"><code>{tx.transactionHash}</code></td>
-        </tr>
+        <RowToggle eventKey={index.toString()} name={name} tx={tx} />
         {
-          event.event === "VaultUpdated" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Collateral</th>
-                      <th className="col-3" scope="col">Encumbered</th>
-                      <th className="col-3" scope="col">Available</th>
-                      <th className="col-3"/>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="d-flex">
-                      <td className="col-3">{utils.formatEther(event.args[1]).toString()}</td>
-                      <td className="col-3">{utils.formatEther(event.args[2]).toString()}</td>
-                      <td className="col-3">{utils.formatEther(event.args[3]).toString()}</td>
-                      <td className="col-3"/>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
+          (name === "DepositNativeCrypto" || name === "WithdrawNativeCrypto") && (
+            <Accordion.Collapse eventKey={index.toString()} className="border">
+              <div className="d-flex justify-content-around pt-2">
+                <p>Amount: {utils.formatEther(log.args.amount).toString()}</p>
+              </div>
+            </Accordion.Collapse>
           )
         }
         {
-          event.event === "LoanCreated" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0 table-responsive">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Principal</th>
-                      <th className="col-3" scope="col">Collateral</th>
-                      <th className="col-3" scope="col">Timestamp</th>
-                      <th className="col-3" scope="col">Rate</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="d-flex">
-                      <td className="col-3">{utils.formatEther(event.args[0]).toString()}</td>
-                      <td className="col-3">{utils.formatEther(event.args[1]).toString()}</td>
-                      <td className="col-3">{new Date(BigNumber.from(event.args[2]).toNumber() * 1000).toLocaleString()}</td>
-                      <td className="col-3">{utils.formatEther(BigNumber.from(event.args[3]).toString().substring(1, BigNumber.from(event.args[3]).toString().length-9))}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
+          (name === "SupplyModified") && (
+            <Accordion.Collapse eventKey={index.toString()} className="border">
+              <div className="d-flex justify-content-around pt-2">
+                <p>ΔCapital: {utils.formatEther(log.args.capitalAmount).toString()}</p>
+                <p>ΔCollateral: {utils.formatEther(log.args.collAmount).toString()}</p>
+              </div>
+            </Accordion.Collapse>
           )
         }
         {
-          event.event === "Stake" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0 table-responsive">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Capital</th>
-                      <th className="col-3" scope="col">Collateral</th>
-                      <th className="col-3" scope="col">Timestamp</th>
-                      <th className="col-3"/>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="d-flex">
-                      <td className="col-3">{utils.formatEther(event.args[0]).toString()} AUR</td>
-                      <td className="col-3">{utils.formatEther(event.args[1]).toString()} FLR</td>
-                      <td className="col-3">{new Date(BigNumber.from(event.args[2]).toNumber() * 1000).toLocaleString()}</td>
-                      <td className="col-3"/>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
+          (name === "DebtModified") && (
+            <Accordion.Collapse eventKey={index.toString()} className="border">
+              <div className="d-flex justify-content-around pt-2">
+                <p>ΔDebt: {utils.formatEther(log.args.debtAmount).toString()}</p>
+                <p>ΔCollateral: {utils.formatEther(log.args.collAmount).toString()}</p>
+              </div>
+            </Accordion.Collapse>
           )
         }
         {
-          event.event === "Redemption" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0 table-responsive">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Capital</th>
-                      <th className="col-3" scope="col">Collateral</th>
-                      <th className="col-3" scope="col">Timestamp</th>
-                      <th className="col-3"/>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="d-flex">
-                      <td className="col-3">{utils.formatEther(event.args[0]).toString()} AUR</td>
-                      <td className="col-3">{utils.formatEther(event.args[1]).toString()} FLR</td>
-                      <td className="col-3">{new Date(BigNumber.from(event.args[2]).toNumber() * 1000).toLocaleString()}</td>
-                      <td className="col-3"/>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          )
-        }
-        {
-          event.event === "Withdrawal" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0 table-responsive">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Capital</th>
-                      <th className="col-3" scope="col">Collateral</th>
-                      <th className="col-3" scope="col">Timestamp</th>
-                      <th className="col-3"/>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="d-flex">
-                      <td className="col-3">{utils.formatEther(event.args.capital).toString()}</td>
-                      <td className="col-3">{utils.formatEther(event.args.collateral).toString()} FLR</td>
-                      <td className="col-3">{new Date(BigNumber.from(event.args.timestamp).toNumber() * 1000).toLocaleString()}</td>
-                      <td className="col-3"/>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          )
-        }
-        {
-          event.event === "Repayment" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Amount</th>
-                      <th className="col-3" scope="col">Collateral</th>
-                      <th className="col-3" scope="col">Timestamp</th>
-                      <th className="col-3"/>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="d-flex">
-                      <td className="col-3">{utils.formatEther(event.args[0]).toString()}</td>
-                      <td className="col-3">{utils.formatEther(event.args[1]).toString()}</td>
-                      <td className="col-3">{new Date(BigNumber.from(event.args[2]).toNumber() * 1000).toLocaleString()}</td>
-                      <td className="col-3"/>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          )
-        }
-        {
-          event.event === "Approval" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Value</th>
-                      <th className="col-3" scope="col">Spender</th>
-                    </tr>
-                  </thead>
-                <tbody>
-                  <tr className="d-flex">
-                    <td className="col-3">{utils.formatEther(event.args.value).toString()}</td>
-                    <td className="col-3">{event.args.spender}</td>
-                  </tr>
-                </tbody>
-                </table>
-              </td>
-            </tr>
-          )
-        }
-        {
-          event.event === "Liquidation" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-3" scope="col">Owner</th>
-                      <th className="col-3" scope="col">Coll. Amt.</th>
-                      <th className="col-3" scope="col">Coll. Val.</th>
-                      {/* <th className="col-3" scope="col">Keeper</th> */}
-                      <th className="col-3" scope="col">Timestamp</th>
-                    </tr>
-                  </thead>
-                <tbody>
-                  <tr className="d-flex">
-                    <td className="col-3 text-truncate">{event.args.borrower}</td>
-                    <td className="col-3">{numeral(utils.formatEther(event.args.collateralAmount)).format('0,0.0[00000000000000000]')}</td>
-                    <td className="col-3">{numeral(utils.formatEther(event.args.collateralValue)).format('$0,0.00')}</td>
-                    {/* <td className="col-3">{event.args.liquidator}</td> */}
-                    <td className="col-3">{new Date(BigNumber.from(event.args.timestamp).toNumber() * 1000).toLocaleString()}</td>
-                  </tr>
-                </tbody>
-                </table>
-              </td>
-            </tr>
-          )
-        }
-        {
-          event.event === "NewPendingTransferToXRP" && (
-            <tr className="collapse" id={`collapse-${index}`}>
-              <td colSpan={4}>
-                <table className="table table-borderless mb-0">
-                  <thead>
-                    <tr className="d-flex">
-                      <th className="col-9" scope="col">ID</th>
-                      <th className="col-3" scope="col">Amount</th>
-                    </tr>
-                  </thead>
-                <tbody>
-                  <tr className="d-flex">
-                    <td className="col-9 text-truncate"><code>{event.args.txHash}</code></td>
-                    <td className="col-3">{numeral(utils.formatEther(event.args.amount)).format('0,0.0[00000000000000000]')}</td>
-                  </tr>
-                </tbody>
-                </table>
-              </td>
-            </tr>
+          (name === "WithdrawAurei") && (
+            <Accordion.Collapse eventKey={index.toString()} className="border">
+              <div className="d-flex justify-content-around pt-2">
+                <p>Amount: {utils.formatEther(log.args.amount).toString()}</p>
+              </div>
+            </Accordion.Collapse>
           )
         }
       </React.Fragment>
@@ -279,19 +96,21 @@ export default function Transactions() {
               </span>
             </div>
           ) : (
-            <table className="table table-bordered">
-              <thead>
-                <tr className="d-flex text-center">
-                  <th className="col-1" scope="col">#</th>
-                  <th className="col-3" scope="col">Event Type</th>
-                  <th className="col-1" scope="col">Block</th>
-                  <th className="col-7" scope="col">Transaction Hash</th>
-                </tr>
-              </thead>
-              <tbody>
-                {rows}
-              </tbody>
-            </table>
+            <Accordion defaultActiveKey={"0"}>
+              <table className="table table-bordered">
+                <thead>
+                  <tr className="d-flex text-center">
+                    <th className="col-1" scope="col">#</th>
+                    <th className="col-3" scope="col">Event Type</th>
+                    <th className="col-1" scope="col">Block</th>
+                    <th className="col-7" scope="col">Transaction Hash</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {rows}
+                </tbody>
+              </table>
+            </Accordion>
           )}
         </div>
       </section>
