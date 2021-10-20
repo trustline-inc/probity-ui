82bcb565156676640ccb72661d7a853d15646acd
Matthew Rosendin <matt@trustline.co>
2021-10-19 17:46:24 -0700

    add flare chain id

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 export const getNativeTokenSymbol = (chainId: number) => {
   switch(chainId) {
+    case 14:
+      return "FLR"
     case 16:
       return "FLR"
     case 19:
@@ -11,6 +13,8 @@ export const getNativeTokenSymbol = (chainId: number) => {
 
 export const getStablecoinSymbol = (chainId: number) => {
   switch(chainId) {
+    case 14:
+      return "AUR"
     case 16:
       return "AUR"
     case 19:
