5d34783424c4ac4bed5c5f56fa2c6f4b09b5c881
Matthew Rosendin <matt@trustline.co>
2021-12-30 11:24:29 -0800

    temp: hide transfers tab

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -146,11 +146,11 @@ function Navbar() {
               <i className="fas fa-hand-holding-usd"></i> Loans
             </NavLink>
           </li>
-          <li className="nav-item my-1">
+          {/* <li className="nav-item my-1">
             <NavLink className="nav-link" activeClassName="active" to="/transfers">
               <i className="fas fa-paper-plane" /> Transfers
             </NavLink>
-          </li>
+          </li> */}
           <li className="nav-item my-1">
             <NavLink className="nav-link" activeClassName="active" to="/liquidations">
               <i className="fas fa-hand-holding-water" /> Liquidations
