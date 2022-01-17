b063f31e60d04b24b865fd15ca93ffbb7e89b7e7
Matthew Rosendin <matt@trustline.co>
2022-01-16 18:04:19 -0800

    remove coll

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ export const getAssetId = (hash: string) => {
     "0x34f0798059a90d875925407ee1d283ae0650aaa8085071fb70c8c42029adc150": "SGB",
     "0x3394a96ae3d76664d9a21dad8ede426bf36b6faee767e047d6aaf44653934fa9": "CFLR"
   }[hash]
-  return id || "UNRECOGNIZED_COLL_ID"
+  return id || "UNRECOGNIZED_ID"
 }
 
 export const getNativeTokenSymbol = (chainId: number) => {
